36b3467e30f3f539276393702a71207c78659e47
pc-b114-15\Eleve <antoine.depercin@student-cs.fr>
2022-12-01 12:03:27 +0100

    Ajout de Grille & case

--- BatailleNavale_MEDEV/Grille.cpp
+++ BatailleNavale_MEDEV/Grille.cpp
@@ -1,2 +1,16 @@
 
 #include "Grille.h"
+
+
+void Grille::afficheAll() {
+	afficheGrille();
+	// Affiche les objets dans la grille
+	for (int i = 0; i < caseVect.size(); i++)
+	{
+		(*caseVect[i]).affiche();
+	}
+}
+
+void Grille::afficheGrille() {
+	// Affiche la grille (les lignes verticales & horizontales)
+}
--- BatailleNavale_MEDEV/Grille.h
+++ BatailleNavale_MEDEV/Grille.h
@@ -1,6 +1,18 @@
 #pragma once
 
 #include <iostream>
+#include <vector>
+
 #include "Position.h"
+#include "Case.h"
 
 using namespace std;
+
+
+class Grille {
+	vector<Case*> caseVect;
+public:
+	Grille() {};
+	void afficheAll();
+	void afficheGrille();
+};
