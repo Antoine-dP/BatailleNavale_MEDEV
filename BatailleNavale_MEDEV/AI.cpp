4c14ebf3a09d9d8aff265984520470d6bd39288d
tecochard <theo.ecochard@eleves.ec-nantes.fr>
2023-01-05 09:53:34 +0100

    Ajout D'un constructeur D'AI + Vector Position disponible

--- BatailleNavale_MEDEV/AI.cpp
+++ BatailleNavale_MEDEV/AI.cpp
@@ -1,5 +1,18 @@
 #include "AI.h"
 
+AI::AI(int _difficulte) {
+	difficulte = _difficulte;
+	essai = NULL;
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			Position pos;
+			pos.x = i;
+			pos.y = j;
+			dispo.push_back(pos);
+		}
+	}
+}
+
 Position AI::jouer() {
 	//TODO (non fini)
 
--- BatailleNavale_MEDEV/AI.h
+++ BatailleNavale_MEDEV/AI.h
@@ -1,10 +1,14 @@
 #pragma once
 #include "Position.h"
 #include "Joueur.h"
+#include <vector>
 
 class AI : public Joueur {
 private:
 	int difficulte;
+	vector<Position> essai;
+	vector<Position> dispo;
 public:
+	AI(int _difficulte);
 	Position jouer();
 };
