ca8449f3d8194455ebba03350549b2915fac86da
Edouuu <fr1310edouard@gmail.com>
2023-01-05 11:45:24 +0100

    Enleve etat d'une case

--- BatailleNavale_MEDEV/Case.h
+++ BatailleNavale_MEDEV/Case.h
@@ -9,19 +9,13 @@ using namespace std;
 
 class Case {
 	Position position;
-	int etat;
-	// 0 : vide
-	// 1 : bateau non touché
-	// 2 : bateau touché
 public:
 	Case(Position p, int _etat) {
 		position = p;
-		etat = _etat;
 	}
 	Case(int x, int y, int _etat) {
 		position.x = x;
 		position.y = y;
-		etat = _etat;
 	}
 	//virtual void affiche() = 0;
 };
