ed4ac8e21540f9d26dfd38dc7c6d8a0903c178b3
tecochard <theo.ecochard@eleves.ec-nantes.fr>
2023-01-05 10:24:56 +0100

    Ajout Fonction jouer pour IA difficulte 0

--- BatailleNavale_MEDEV/AI.cpp
+++ BatailleNavale_MEDEV/AI.cpp
@@ -1,11 +1,21 @@
 #include "AI.h"
+#include <random>
 
 Position AI::jouer() {
 	//TODO (non fini)
 
 	// IA nulle : random position
 	if (difficulte == 0) {
+		// Génère un nombre aléatoire entre 0 et la taille du tableau
+		int index = rand() % dispo.size();
 
+		// Enregistre l'élément à enlever dans la variable de résultat
+		Position posChoisie = dispo[index];
+
+		// Enlève l'élément du tableau
+		dispo.erase(dispo.begin() + index);
+
+		return posChoisie;
 	}
 
 	//CODE A RETIRER EN BAS
--- BatailleNavale_MEDEV/AI.h
+++ BatailleNavale_MEDEV/AI.h
@@ -1,10 +1,12 @@
 #pragma once
 #include "Position.h"
 #include "Joueur.h"
+#include <vector>
 
 class AI : public Joueur {
 private:
 	int difficulte;
+	vector<Position> dispo;
 public:
 	Position jouer();
 };
