0cab596e063c45e914f8ee10fa652479c3ad75ba
Edouuu <fr1310edouard@gmail.com>
2023-01-12 10:41:33 +0100

    Update Joueur.cpp
    Fonction placerBateaux

--- BatailleNavale_MEDEV/Joueur.cpp
+++ BatailleNavale_MEDEV/Joueur.cpp
@@ -17,17 +17,71 @@ void Joueur::placerBateaux() {
 	bool possible = false;
 
 	// Bateau 1 : (Porte Avion 5 cases)
-	cout << "Placement Porte Avion (5 cases) : " << endl;
-	cout << "Orientation (h ou v) : ";
-	cin >> orientation;
-	cout << endl;
-	cout << "Position en x : ";
-	cin >> posX;
-	cout << endl << "Position en y : ";
-	cin >> posY;
-	grilleJ.placerBateau(5, orientation, posX, posY);
+	while (!possible) {
+		cout << "Placement Porte Avion (5 cases) : " << endl;
+		cout << "Orientation (h ou v) : ";
+		cin >> orientation;
+		cout << endl;
+		cout << "Position en x : ";
+		cin >> posX;
+		cout << endl << "Position en y : ";
+		cin >> posY;
+		possible = grilleJ.placerBateau(5, orientation, posX, posY);
+	}
 
+	possible = false;
+	// Bateau 2 : (Croiseur 4 cases)
+	while (!possible) {
+		cout << "Placement Croiseur (4 cases) : " << endl;
+		cout << "Orientation (h ou v) : ";
+		cin >> orientation;
+		cout << endl;
+		cout << "Position en x : ";
+		cin >> posX;
+		cout << endl << "Position en y : ";
+		cin >> posY;
+		possible = grilleJ.placerBateau(4, orientation, posX, posY);
+	}
 
+	possible = false;
+	// Bateau 3 : (Sous-marin 3 cases)
+	while (!possible) {
+		cout << "placement Sous-Marin (3 cases) : " << endl;
+		cout << "Orientation (h ou v) : ";
+		cin >> orientation;
+		cout << endl;
+		cout << "Position en x : ";
+		cin >> posX;
+		cout << endl << "Position en y : ";
+		cin >> posY;
+		possible = grilleJ.placerBateau(4, orientation, posX, posY);
+	}
 
+	possible = false;
+	// Bateau 4 : (Sous-marin 3 cases)
+	while (!possible) {
+		cout << "Placement Sous-Marin (3 cases) : " << endl;
+		cout << "Orientation (h ou v) : ";
+		cin >> orientation;
+		cout << endl;
+		cout << "Position en x : ";
+		cin >> posX;
+		cout << endl << "Position en y : ";
+		cin >> posY;
+		possible = grilleJ.placerBateau(4, orientation, posX, posY);
+	}
 
+	possible = false;
+	// Bateau 5 : (Torpilleur 2 cases)
+	while (!possible) {
+		cout << "Placement Torpilleur (2 cases) : " << endl;
+		cout << "Orientation (h ou v) : ";
+		cin >> orientation;
+		cout << endl;
+		cout << "Position en x : ";
+		cin >> posX;
+		cout << endl << "Position en y : ";
+		cin >> posY;
+		possible = grilleJ.placerBateau(4, orientation, posX, posY);
+	}
 }
